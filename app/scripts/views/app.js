6e4b801a52788ac0a0189a534171f39c72ae5677
Franco Zanki <zankian.vision@gmail.com>
2013-05-04 09:52:42 +0100

    refactoring slides.js

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -13,6 +13,8 @@ define([
     
     var AppView = Backbone.View.extend({
 
+        el: 'body',
+
         initialize: function() {
             var testCollection = [
                 {title: "My First Slide"},
@@ -26,6 +28,20 @@ define([
 
             App.router = new MainRouter();
             Backbone.history.start();
+        },
+
+        events: {
+            'keyup': 'keyUp'
+        },
+
+        keyUp: function(e) {
+            // 37 left 
+            // 39 right
+            if (e.keyCode === 37 || e.keyCode === 39) {
+                App.Vent.trigger('changeSlide', {
+                    direction: e.keyCode === 39 ? 'next' : 'prev'
+                });
+            }
         }
 
 
--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -6,7 +6,88 @@ define(['backbone', 'views/slide'], function(Backbone, SlideView){
 		el: $('.slides'),
 
 		initialize: function() {
+
+			this.currentSlideIndex = 1;
+			this.numSlides = this.collection.length;
+			this.transitionSpeed = 400;
+			
 			this.renderAll();
+
+			App.Vent.on('init', this.hideAllButFirst, this);
+			App.Vent.on('changeSlide', this.changeSlide, this);
+		},
+
+		hideAllButFirst: function() {
+			this.$el.children(':nth-child(n+2)').hide();
+		},
+
+		changeSlide: function(opts) {
+
+			var newSlide;
+			var slides = this.$el.children();
+			var self = this;
+			console.log(opts);
+			
+
+			this.setCurrentSlideIndex(opts);
+			
+			// fileter the new slide
+			/// VERY IMPORTANT !!! THIS IS WHRE ALL THE LOGGIC IS HAPPENING 
+			/// AND FIGURING WHICH SLIDE WE ARE WORKING ON
+			newSlide = slides.eq(this.currentSlideIndex - 1);
+
+			newSlide = this.getNextSlide(slides);
+			//console.log( newSlide )
+			slides.css('position','absolute')// TEMPORARY
+
+			this.animateToNewSlide(slides, newSlide, opts.direction);
+
+			App.router.navigate('/slides/' + this.currentSlideIndex);
+		},
+
+		setCurrentSlideIndex: function(opts) {
+
+			if (opts.slideIndex) {
+				//debugger;
+				return this.currentSlideIndex += ~~opts.slideIndex;
+			}
+
+			this.currentSlideIndex += (opts.direction === 'next') ? 1 : -1;
+			
+			if( this.currentSlideIndex > this.numSlides ) {
+				// go back to slide 1
+				this.currentSlideIndex = 1;
+			}
+			if ( this.currentSlideIndex <= 0) {
+				
+				this.currentSlideIndex = this.numSlides;
+			}
+		},
+
+		animateToNewSlide: function(slides, newSlide, direction) {
+
+			slides.filter(':visible')
+				.animate({
+					top: direction === 'next' ? '100%' : '-100%',
+					//top: '-100%',
+					opacity: 'hide'
+				}, this.transitionSpeed, function() {
+					$(this).css('top','0');
+
+					newSlide
+						.css('position','absolute') // TEMPORARY
+						.css('top', direction ==='next' ? '-100%' : '100%')
+						.animate({
+							top:0,
+							opacity: 'show'
+						}, self.transitionSpeed);
+				});
+
+		},
+
+		getNextSlide: function(slides) {
+			return slides.eq(this.currentSlideIndex - 1);
+
 		},
 
 		renderAll: function() {
